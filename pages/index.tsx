bed9c1649a60c36fce69c531db083dd09c7889ff
happy-sonday <mrsmiler1106@gmail.com>
2021-06-15 23:07:32 +0900

    global css 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,24 @@
 import { NextPage } from "next";
-
+import React from "react";
+import styled from "styled-components";
+import GlobalStyle from "../styles/GlobalStyle";
+const Container = styled.div`
+    padding: 20px;
+`;
 const index : NextPage= ()=>{
-    return <div>Hello typescript! update syntex</div>
+
+    return (
+    <>
+    <GlobalStyle />
+    <Container>Hello typescript! update syntex
+    <h1>Hello typescript! update syntex</h1>
+    <h2>Hello typescript! update syntex</h2>
+    <h3>Hello typescript! update syntex</h3>
+    <a>안녕하세요</a>
+    </Container>
+    </>
+    );
+
 }
 
 export default index;
