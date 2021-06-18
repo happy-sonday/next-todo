ab13c09ed987e17f46f69bf527e8da450516575c
happy-sonday <mrsmiler1106@gmail.com>
2021-06-19 08:56:28 +0900

    headers 제작 및 반영

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import { AppProps } from "next/dist/next-server/lib/router/router";
 import React from "react";
+import Header from "../components/Header";
 import GlobalStyle from "../styles/GlobalStyle";
 
 const app = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <GlobalStyle />
+      <Header />
       <Component {...pageProps} />
     </>
   );
