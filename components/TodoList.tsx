5bd357aa05b4d76c2b800c8137837144a6da4a54
happy-sonday <mrsmiler1106@gmail.com>
2021-06-19 14:36:26 +0900

    TODO List 목록 퍼블리싱 퍼블리싱 임임시 커밋

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 import styled from "styled-components";
 import { TodoType } from "../types/todo";
 import palette from "../styles/pallete";
+
+type ObjectIndexType = {
+  [key: string]: number | undefined;
+};
 
 interface IProps {
   todos: TodoType[];
@@ -10,7 +14,7 @@ interface IProps {
 const Container = styled.div`
   width: 100%;
 
-  /* .todo-list-header {
+  .todo-list-header {
     padding: 12px;
     border-bottom: 1px solid ${palette.gray};
 
@@ -20,7 +24,105 @@ const Container = styled.div`
         margin-left: 8px;
       }
     }
-  } */
+  }
+
+  //NOTE:색깔 라벨링 및 카운트
+  .todo-list-header-colors {
+    display: flex;
+    .todo-list-header-color-num {
+      display: flex;
+      margin-right: 8px;
+      p {
+        font-size: 14px;
+        line-height: 16px;
+        margin: 0;
+        margin-left: 6px;
+      }
+      .todo-list-header-round-color {
+        width: 16px;
+        height: 16px;
+        border-radius: 50%;
+      }
+    }
+  }
+  .bg-blue {
+    background-color: ${palette.blue};
+  }
+  .bg-green {
+    background-color: ${palette.green};
+  }
+  .bg-navy {
+    background-color: ${palette.navy};
+  }
+  .bg-orange {
+    background-color: ${palette.orange};
+  }
+  .bg-red {
+    background-color: ${palette.red};
+  }
+  .bg-yellow {
+    background-color: ${palette.yellow};
+  }
+
+  //NOTE: 리스트 스타일링
+
+  .todo-list {
+    .todo-item {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 100%;
+      height: 52px;
+      border-bottom: 1px solid ${palette.gray};
+
+      .todo-left-side {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        align-items: center;
+        .todo-color-block {
+          width: 12px;
+          height: 100%;
+        }
+        .checked-todo-text {
+          color: ${palette.gray};
+          text-decoration: line-through;
+        }
+        .todo-text {
+          margin-left: 12px;
+          font-size: 16px;
+        }
+      }
+    }
+  }
+
+  .todo-right-side {
+    display: flex;
+    align-items: center;
+    margin-right: 12px;
+    svg {
+      &:first-child {
+        margin-right: 16px;
+      }
+    }
+    .todo-trash-can {
+      width: 16px;
+      path {
+        fill: ${palette.deep_red};
+      }
+    }
+    .todo-check-mark {
+      fill: ${palette.deep_green};
+    }
+    .todo-button {
+      width: 20px;
+      height: 20px;
+      border-radius: 50%;
+      border: 1px solid ${palette.gray};
+      background-color: transparent;
+      outline: none;
+    }
+  }
 `;
 
 const TodoHeader = styled.div`
@@ -36,19 +138,101 @@ const TodoListLastNumber = styled.p`
 `;
 
 const ToDoList: React.FC<IProps> = ({ todos }) => {
+  //* 색깔 객체 구하기 1
+  const getTodoColorNums = useCallback(() => {
+    let red = 0;
+    let orange = 0;
+    let yellow = 0;
+    let green = 0;
+    let blue = 0;
+    let navy = 0;
+    todos.forEach((todo) => {
+      switch (todo.color) {
+        case "red":
+          red += 1;
+          break;
+        case "orange":
+          orange += 1;
+          break;
+        case "yellow":
+          yellow += 1;
+          break;
+        case "green":
+          green += 1;
+          break;
+        case "blue":
+          blue += 1;
+          break;
+        case "navy":
+          navy += 1;
+          break;
+        default:
+          break;
+      }
+    });
+
+    return {
+      red,
+      orange,
+      yellow,
+      green,
+      blue,
+      navy
+    };
+  }, [todos]);
+
+  //* 객체의 문자열 인덱스 사용을 위한 타입
+  type ObjectIndexType = {
+    [key: string]: number | undefined;
+  };
+
+  //* 색깔 객체 구하기 2
+  const todoColorNums = useMemo(() => {
+    const colors: ObjectIndexType = {};
+    todos.forEach((todo) => {
+      const value = colors[todo.color];
+      if (!value) {
+        //* 존재하지않던 key라면
+        colors[`${todo.color}`] = 1;
+      } else {
+        //* 존재하는 키라면
+        colors[`${todo.color}`] = value + 1;
+      }
+    });
+    return colors;
+  }, [todos]);
+
   return (
     <Container>
-      {/*  <div className="todo-list-header">
+      <div className="todo-list-header">
         <p className="todo-list-last-todo">
-          남은 TODO<span>{todos.length}</span>
+          남은TODO<span>{todos.length}개</span>
         </p>
-      </div> */}
+        <div className="todo-list-header-colors">
+          {Object.keys(todoColorNums).map((color, index) => (
+            <div className="todo-list-header-color-num" key={index}>
+              <div className={`todo-list-header-round-color bg-${color}`} />
+              <p>{todoColorNums[color]}개</p>
+            </div>
+          ))}
+        </div>
+      </div>
 
-      <TodoHeader>
-        <TodoListLastNumber>
-          남은 TODO<span>{todos.length}</span>
-        </TodoListLastNumber>
-      </TodoHeader>
+      <ul className="todo-list">
+        {todos.map((todo) => (
+          <li className="todo-item" key={todo.id}>
+            {/* NOTE:리스트-왼쪽 */}
+            <div className="todo-left-side">
+              <div className={`todo-color-block bg-${todo.color}`} />
+              <p className={`todo-text ${todo.checked ? "checked-todo-text" : ""}`}>{todo.text}</p>
+            </div>
+            {/* NOTE:리스트-오른쪽 */}
+            <div className="todo-right-side">
+              {!todo.checked && <button type="button" className="todo-button" onClick={() => {}} />}
+            </div>
+          </li>
+        ))}
+      </ul>
     </Container>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import { NextPage } from "next";
-import React from "react";
+import React, { useCallback, useMemo } from "react";
 import styled from "styled-components";
 import TodoList from "../components/TodoList";
 import GlobalStyle from "../styles/GlobalStyle";
@@ -9,7 +9,12 @@ const Container = styled.div`
   padding: 20px;
 `;
 
-const todos: TodoType[] = [{ id: 1, text: "마트 가서 장보기", color: "red", checked: false }];
+const todos: TodoType[] = [
+  { id: 1, text: "마트 가서 장보기", color: "red", checked: false },
+  { id: 2, text: "정보처리기사 실기 공부", color: "orange", checked: true },
+  { id: 3, text: "리액트 에제 실습", color: "yellow", checked: false },
+  { id: 4, text: "화면 개선 및 서비스 기획", color: "blue", checked: false }
+];
 
 const index: NextPage = () => {
   // 해당 컴포넌트를 받을 제네릭 타입으로 설정하지않으면 No quick fixes availble Error 발생
