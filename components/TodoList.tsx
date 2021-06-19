3241f290aa97c9ea569bec4068a33de411c744e4
happy-sonday <mrsmiler1106@gmail.com>
2021-06-19 13:02:52 +0900

    스타일드 컴포넌트

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,14 +1,54 @@
 import React from "react";
 import styled from "styled-components";
+import { TodoType } from "../types/todo";
+import palette from "../styles/pallete";
+
+interface IProps {
+  todos: TodoType[];
+}
 
 const Container = styled.div`
   width: 100%;
+
+  /* .todo-list-header {
+    padding: 12px;
+    border-bottom: 1px solid ${palette.gray};
+
+    .todo-list-last-todo {
+      font-size: 14px;
+      span {
+        margin-left: 8px;
+      }
+    }
+  } */
 `;
 
-const ToDoList: React.FC = () => {
+const TodoHeader = styled.div`
+  padding: 12px;
+  border-bottom: 1px solid ${palette.gray};
+`;
+
+const TodoListLastNumber = styled.p`
+  font-size: 14px;
+  span {
+    margin-left: 8px;
+  }
+`;
+
+const ToDoList: React.FC<IProps> = ({ todos }) => {
   return (
     <Container>
-      <h1>TodoList</h1>
+      {/*  <div className="todo-list-header">
+        <p className="todo-list-last-todo">
+          남은 TODO<span>{todos.length}</span>
+        </p>
+      </div> */}
+
+      <TodoHeader>
+        <TodoListLastNumber>
+          남은 TODO<span>{todos.length}</span>
+        </TodoListLastNumber>
+      </TodoHeader>
     </Container>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,19 @@
 import { NextPage } from "next";
 import React from "react";
 import styled from "styled-components";
+import TodoList from "../components/TodoList";
 import GlobalStyle from "../styles/GlobalStyle";
+import { TodoType } from "../types/todo";
 
 const Container = styled.div`
   padding: 20px;
 `;
+
+const todos: TodoType[] = [{ id: 1, text: "마트 가서 장보기", color: "red", checked: false }];
+
 const index: NextPage = () => {
-  return (
-    <Container>
-      <h1>hello Styled-components</h1>
-      <h2>hello Styled-components</h2>
-      <h3>hello Styled-components</h3>
-    </Container>
-  );
+  // 해당 컴포넌트를 받을 제네릭 타입으로 설정하지않으면 No quick fixes availble Error 발생
+  return <TodoList todos={todos} />;
 };
 
 export default index;
