d8fcbb4ef6d6ec1f5862d90000f1ce5f3cb6d17b
happy-sonday <mrsmiler1106@gmail.com>
2021-06-20 23:23:37 +0900

    post get test

--- pages/api/todos.ts
+++ pages/api/todos.ts
@@ -1,5 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  return res.send("hello next api!!");
+  if (req.method === "GET") {
+    return res.send("hello GET api");
+  }
+
+  res.statusCode = 405;
+  console.log(res.statusCode);
+  return res.end();
 };
